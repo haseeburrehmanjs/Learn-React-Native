929143c60d85bf169e78d75bcdf7f0bb40c04a22
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-10-25 00:44:08 +0500

    dynamic-project-created

---
+++ dynamic-routing/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text className='text-center text-4xl'>index</Text>
+    </View>
+  )
+}
+
+export default index
