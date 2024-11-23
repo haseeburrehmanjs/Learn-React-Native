f8fa59c50d68acebbc7ad6a603be3d3f6d6476fe
haseeburrehmanjs <kamranking786500@gmail.com>
2024-11-23 20:10:22 +0500

    modified work

--- hello-world/app/index.tsx
+++ hello-world/app/index.tsx
@@ -28,6 +28,12 @@ const index = () => {
         <Link style={{
           color: 'white'
         }} href={'contact'}>Contact</Link>
+        <Link style={{
+          color: 'white'
+        }} href={'contact'}>Services</Link>
+        <Link style={{
+          color: 'white'
+        }} href={'contact'}>About Us</Link>
     </View>
   )
 }
